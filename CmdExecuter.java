c739e4e371d6a95a1319ecfc7b5e82e662d0d63e
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-09-15 13:24:59 +0200

    OS-ambiguity back

--- CmdExecuter.java
+++ CmdExecuter.java
@@ -98,81 +98,8 @@ public class CmdExecuter implements Runnable
     public void execute() 
     {
         cmd = cmd.trim(); // Remove whitespaces before n after as needed.
-        if (cmd.startsWith("ls")) // ls
-        {
-            Path dir = Paths.get(cwd);
-            String spacing = "\t";
-            if (cmd.contains("-l"))
-                spacing = "\n";
-//            System.out.println("Path: "+dir.toString());
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*")) 
-            {
-                for (Path file : stream) {
-                    output += file.getFileName() + spacing;
-                }
-            } catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-            return;
-        }
-        if (cmd.startsWith("pwd"))
-        {
-            System.out.println(cwd);
-            return;
-        }
-        if (cmd.startsWith("cd"))
-        {
-            if (cmd.length() < 3){
-                System.out.println("Forgot to provide directory to cd command?");
-                return;
-            }
-            cwd = cmd.substring(3); // from "cd " and to the end of this cmd?
-            return;
-        }
-        if (cmd.startsWith("cat"))
-        {
-            // Read file.
-            try{
-                String file = cmd.substring(4);
-                String path = cwd+"/"+file;
-                System.out.println("File: "+path);
-                BufferedReader br = new BufferedReader(new FileReader(path));
-                try {
-                    StringBuilder sb = new StringBuilder();
-                    String line = br.readLine();
-
-                    while (line != null) {
-                        sb.append(line);
-                        sb.append(System.lineSeparator());
-                        line = br.readLine();
-                    }
-                    String everything = sb.toString();
-                    output = everything;
-                } 
-                finally {
-                    br.close();
-                }
-            }
-            catch (Exception e)
-            {
-                System.out.println(e.toString());
-                e.printStackTrace();
-                failed = true;
-            } 
-            return;
-        }
-        /// Not working yet.
-        if (cmd.startsWith("uptime"))
-        {
-            uptime();
-            return;
-        }
-        output = "Unsupported command: "+cmd;
-        failed = true;
-//        System.out.println();
-        return;
-       
+        RunOSCmd();
+        return;       
     }
     /// Runs the command natively in the OS. 
     void RunOSCmd()
@@ -204,9 +131,10 @@ public class CmdExecuter implements Runnable
             System.exit(0);
         }
         catch (IOException e) {
-            System.out.println("exception happened - here's what I know: ");
-            e.printStackTrace();
-            System.exit(-1);
+            System.out.println(e.toString());
+ //           System.out.
+//            e.printStackTrace();
+//            System.exit(-1);
         }    
     };
     /// Like pwd in Linux, fetches current directory.
@@ -249,55 +177,5 @@ public class CmdExecuter implements Runnable
             System.out.println(output);
         }
     }
-    
-    
-    /// Detailed/longer functions to implement some commands.
-    void uptime()
-    {
-        try 
-        {
-            long uptime = -1;
-            String os = System.getProperty("os.name").toLowerCase();
-            if (os.contains("win")) {
-                Process uptimeProc = Runtime.getRuntime().exec("net stats srv");
-                BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
-                String line;
-                while ((line = in.readLine()) != null) {
-                    if (line.startsWith("Statistics since")) 
-                    {
-                        SimpleDateFormat format = new SimpleDateFormat("'Statistics since' yyyy-MM-dd hh:mm:ss");
-                        Date boottime = format.parse(line);
-                        uptime = System.currentTimeMillis() - boottime.getTime();
-                        break;
-                    }
-                }
-            } else if (os.contains("mac") || os.contains("nix") || os.contains("nux") || os.contains("aix")) {
-                Process uptimeProc = Runtime.getRuntime().exec("uptime");
-                BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
-                String line = in.readLine();
-                if (line != null) {
-                    Pattern parse = Pattern.compile("((\\d+) days,)? (\\d+):(\\d+)");
-                    Matcher matcher = parse.matcher(line);
-                    if (matcher.find()) {
-                        String _days = matcher.group(2);
-                        String _hours = matcher.group(3);
-                        String _minutes = matcher.group(4);
-                        int days = _days != null ? Integer.parseInt(_days) : 0;
-                        int hours = _hours != null ? Integer.parseInt(_hours) : 0;
-                        int minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
-                        uptime = (minutes * 60000) + (hours * 60000 * 60) + (days * 6000 * 60 * 24);
-                    }
-                }
-            }
-            output = "Uptime: "+uptime;
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-    }
-    
-    
-    
-    
+   
 }
