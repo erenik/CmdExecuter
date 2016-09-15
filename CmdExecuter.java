f01eb7c3a45be0272ca6737c6fdaee7eb68efa76
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-09-15 12:41:41 +0200

    Made public + cat added

--- CmdExecuter.java
+++ CmdExecuter.java
@@ -6,13 +6,19 @@
 
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -26,8 +32,9 @@ public class CmdExecuter implements Runnable
     String input; // Input from previous command? if piping to file for example.
     String output; // Std output of the command.
     String cwd; // Current working directory.
+    boolean failed = false; // For unsupported or failures from exceptions. To halt unit- or repeated tests as needed.
     
-    CmdExecuter(String cmd)
+    public CmdExecuter(String cmd)
     {
         initialCmd = cmd;
         cwd = workingDir();
@@ -60,8 +67,9 @@ public class CmdExecuter implements Runnable
             "pwd",
             "cd",
             "cd /home/ && pwd | ls –all ",
-            "uptime", /// uptime and below have not been implemented yet.
-            "cat file.txt",
+            /// below have not been implemented yet.
+            "cat src/README.md",
+            "uptime", 
             "who",
             "ps",
             "grep",
@@ -71,6 +79,8 @@ public class CmdExecuter implements Runnable
         {
             CmdExecuter cmd = new CmdExecuter(unitTests[i]);
             cmd.run();
+            if (cmd.failed)
+                break;
         }
         
         // Get input
@@ -120,7 +130,46 @@ public class CmdExecuter implements Runnable
             cwd = cmd.substring(3); // from "cd " and to the end of this cmd?
             return;
         }
+        if (cmd.startsWith("cat"))
+        {
+            // Read file.
+            try{
+                String file = cmd.substring(4);
+                String path = cwd+"/"+file;
+                System.out.println("File: "+path);
+                BufferedReader br = new BufferedReader(new FileReader(path));
+                try {
+                    StringBuilder sb = new StringBuilder();
+                    String line = br.readLine();
+
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                        line = br.readLine();
+                    }
+                    String everything = sb.toString();
+                    output = everything;
+                } 
+                finally {
+                    br.close();
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.toString());
+                e.printStackTrace();
+                failed = true;
+            } 
+            return;
+        }
+        /// Not working yet.
+        if (cmd.startsWith("uptime"))
+        {
+            uptime();
+            return;
+        }
         output = "Unsupported command: "+cmd;
+        failed = true;
 //        System.out.println();
         return;
        
@@ -200,6 +249,54 @@ public class CmdExecuter implements Runnable
             System.out.println(output);
         }
     }
+    
+    
+    /// Detailed/longer functions to implement some commands.
+    void uptime()
+    {
+        try 
+        {
+            long uptime = -1;
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("win")) {
+                Process uptimeProc = Runtime.getRuntime().exec("net stats srv");
+                BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if (line.startsWith("Statistics since")) 
+                    {
+                        SimpleDateFormat format = new SimpleDateFormat("'Statistics since' yyyy-MM-dd hh:mm:ss");
+                        Date boottime = format.parse(line);
+                        uptime = System.currentTimeMillis() - boottime.getTime();
+                        break;
+                    }
+                }
+            } else if (os.contains("mac") || os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+                Process uptimeProc = Runtime.getRuntime().exec("uptime");
+                BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
+                String line = in.readLine();
+                if (line != null) {
+                    Pattern parse = Pattern.compile("((\\d+) days,)? (\\d+):(\\d+)");
+                    Matcher matcher = parse.matcher(line);
+                    if (matcher.find()) {
+                        String _days = matcher.group(2);
+                        String _hours = matcher.group(3);
+                        String _minutes = matcher.group(4);
+                        int days = _days != null ? Integer.parseInt(_days) : 0;
+                        int hours = _hours != null ? Integer.parseInt(_hours) : 0;
+                        int minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
+                        uptime = (minutes * 60000) + (hours * 60000 * 60) + (days * 6000 * 60 * 24);
+                    }
+                }
+            }
+            output = "Uptime: "+uptime;
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    
     
     
     
