db5331ad08617e4b5df6da13d53f31cd04d2235c
Valentin Poirot <Valentin Poirot>
2016-09-16 10:38:40 +0200

    empty loop class

---
+++ EmptyLoop.java
@@ -0,0 +1,34 @@
+
+public class EmptyLoop extends Thread {
+	
+
+	public static void main(String[] args) {
+		// we check if we receive an argument
+		 if(args.length != 1) {
+				 System.out.println("No arguments were given, exiting.");
+				 System.exit(0);
+		 }
+		 
+		 int iterations = 0;
+		 // we want to avoid format exception
+		 try {
+			 iterations = Integer.parseInt(args[0]);
+		 } catch (Exception e) {
+			 System.out.println("Wrong argument!");
+			 System.out.println(e.getMessage());
+		 }
+		 
+		// we print the number of execution we are going to do
+		System.out.println("Number of iterations: "+iterations);
+		
+		for (int i=0;i<iterations;++i) {
+			// doing nothing
+		}
+		
+		System.out.println(iterations+" iterations done!");
+		
+		
+
+	}
+
+}
